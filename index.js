1845f059dc9613ef7bb76b9a4b31842c75766210
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2023-02-15 11:32:30 +0100

    patch

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const helmet = require('helmet');
-const cors = require('cors');
-
 const fs = require('fs');
-
 const dirname = process.cwd();
 
-var config = {}
+const app = express();
 
-app.use(helmet());
+var config = {}
 
 try {
   const rawdata = fs.readFileSync(path.join(dirname, 'configFrontend.json'));
@@ -21,15 +16,6 @@ catch (e) {
   console.log('config file ->', e.message);
 }
 
-var corsOptions = {
-  "origin": "*",
-  "Access-Control-Request-Method": "POST,GET,PUT,DELETE,OPTIONS",
-  "Access-Control-Allow-Headers": "X-Requested-With,Content-Type",
-  "optionsSuccessStatus": 200
-};
-
-app.use(cors(corsOptions));
-
 const PORT = config.PORT || 8000;
 const HOST = config.HOST || 'localhost';
 
@@ -37,7 +23,6 @@ app.use(express.static(path.join(dirname, 'build')));
 
 app.get('/*', (req, res) => {
    res.sendFile(path.join(dirname, 'build', 'index.html'));
-   //res.json({host: HOST, port: PORT});
 });
 
 app.listen(PORT, HOST, () => {
